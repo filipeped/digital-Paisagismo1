b18233408a90c3120ed6ff7b2379c98a65d44100
filipeped <filipepedoo@gmail.com>
2025-06-27 22:23:22 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
         headers: [
           {
             key: "Access-Control-Allow-Origin",
-            value: "https://www.digitalpaisagismo.online"
+            value: "https://www.digitalpaisagismo.site"
           },
           {
             key: "Access-Control-Allow-Methods",
