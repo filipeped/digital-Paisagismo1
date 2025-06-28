92b6d547de274ae3670c2511f3d38b36f3d153d0
filipeped <filipepedoo@gmail.com>
2025-06-27 22:17:25 -0300

    Update events.ts

--- pages/api/events.ts
+++ pages/api/events.ts
@@ -5,8 +5,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "*");
 
-  if (req.method === "OPTIONS") return res.status(200).end();
-  if (req.method !== "POST") return res.status(405).json({ error: "Method Not Allowed" });
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method Not Allowed" });
+  }
 
   try {
     const ip =
@@ -17,18 +22,19 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone || "America/Sao_Paulo";
     const language = req.headers["accept-language"]?.split(",")[0] || "pt-BR";
+    const userAgent = req.headers["user-agent"] || "";
 
     const payload = {
       ...req.body,
       client_ip_address: ip,
-      client_user_agent: req.headers["user-agent"] || "",
+      client_user_agent: userAgent,
       client_language: language,
       client_timezone: timezone,
       source: req.body?.source || "event_generic",
     };
 
     const response = await fetch(
-      "https://graph.facebook.com/v19.0/3474069809483558/events?access_token=EAAQfmxkTTZCcBOwAhY9Qpfqm0gQ1L3ZAhV3EpsZCSC298vN5oaPMwgdsc6ZCWXFRaBzQlyAHfs4HWlUZAefUh50FGw4gn3NIES5iYFZBoumN2HZAMdVvMh1Wtu9OAunizQoYrZAwJUcFSlZCgvg4HMXp1vgSXQDPBiIadqRO4CgxQU0tCYQBN9JMUMrUY8TMRuQZDZD",
+      "https://graph.facebook.com/v19.0/1771053817142417/events?access_token=EAAQfmxkTTZCcBO7Eg32S6CRuZCKinkgADP8Emq7xH6QebxYmmiaV5ZCC321ETNKrquoWGIz5VF0ZBpVeU2cl7clMf9r1ZCJphdHEycTnnMeFUuZC7rdLhOZA3SobgiCO2gFN27JqIAaYqRuLFLxoq24xZCaEGmEx0FSReKeUaNNLycsDx1cfwizggJZB5kkNINwZDZD",
       {
         method: "POST",
         headers: {
